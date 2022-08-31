a39c713eba56fe87e60a2e163d56f1addfdc511a
surya <suryakondisetti741@gmail.com>
2022-08-31 04:21:11 +0100

    This my typescript basics

---
+++ loops/loops.ts
@@ -0,0 +1,17 @@
+//for loop
+
+let result:string='';
+for(let i=0; i<=10; i++){
+    result+= `${i} `;
+}
+console.log(result);
+
+
+let result1:string='';
+for(let i=20; i>=0; i-=2){
+    result1+= `${i} `;
+}
+
+console.log(result1);
+
+
